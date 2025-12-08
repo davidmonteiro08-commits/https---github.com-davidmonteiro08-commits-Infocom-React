4affb36d1f96480ad12a45c1c07fb35878d6f69f
David Monteiro <david.monteiro08@aluno.ifce.edu.br>
2025-12-07 21:52:41 -0300

    Alteração do return do componente

--- infocom-react/src/components/ProductCard.jsx
+++ infocom-react/src/components/ProductCard.jsx
@@ -2,13 +2,18 @@ import './ProductCard.css';
 
 function ProductCard({ product }) {
   return (
-    <div className="card">
-      <img src={product.image} alt={product.title} />
-      <h3>{product.title}</h3>
-      <div className="price">
-        R$ {product.price.toFixed(2).replace('.', ',')}
-      </div>
-    </div>
+    // Dentro do return do componente ProductCard
+   <div className="card">
+   <img src={product.image} alt={product.title} />
+
+   <div className="separator"></div>
+
+   <div className="price">
+     R$ {product.price.toFixed(2).replace('.', ',')}
+   </div>
+  
+   <h3>{product.title}</h3>
+  </div>
   );
 }
 
