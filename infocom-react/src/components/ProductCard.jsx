0f391ee1b70291e2e3905069d315c72f6c0ee92d
David Monteiro <david.monteiro08@aluno.ifce.edu.br>
2025-12-07 22:24:50 -0300

    Atualização do componente

--- infocom-react/src/components/ProductCard.jsx
+++ infocom-react/src/components/ProductCard.jsx
@@ -1,19 +1,17 @@
 import './ProductCard.css';
+import { useNavigate } from "react-router-dom";
 
 function ProductCard({ product }) {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate(`/products/${product.id}`);
+  };
+
   return (
-    // Dentro do return do componente ProductCard
-   <div className="card">
-   <img src={product.image} alt={product.title} />
-
-   <div className="separator"></div>
-
-   <div className="price">
-     R$ {product.price.toFixed(2).replace('.', ',')}
-   </div>
-  
-   <h3>{product.title}</h3>
-  </div>
+    <div className="card" onClick={handleClick}>
+      {/* Resto do código do card continua igual */}
+    </div>
   );
 }
 
