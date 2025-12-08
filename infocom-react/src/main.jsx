f78d70bd68a9943d4b52531f0e4d40142aa91a6f
David Monteiro <david.monteiro08@aluno.ifce.edu.br>
2025-12-07 22:22:14 -0300

    Atualização do router

--- infocom-react/src/main.jsx
+++ infocom-react/src/main.jsx
@@ -4,6 +4,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import './index.css';
 import App from './App.jsx';
 import Home from "./pages/Home.jsx"; // importação da página Home
+import ProductDetails from "./pages/ProductDetails.jsx"; // importação da página de detalhes
 
 const router = createBrowserRouter([
   {
@@ -13,6 +14,10 @@ const router = createBrowserRouter([
       {
         index: true,
         element: <Home />,
+      },
+      {
+        path: "products/:id",
+        element: <ProductDetails />,
       },
     ],
   },
