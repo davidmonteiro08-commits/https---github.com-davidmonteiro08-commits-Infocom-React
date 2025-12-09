43d32f4dfcd765689981d7b6c0b39ef81bf91375
David Monteiro <david.monteiro08@aluno.ifce.edu.br>
2025-12-09 20:50:47 -0300

    Adição de função

--- infocom-react/src/hooks/useProducts.js
+++ infocom-react/src/hooks/useProducts.js
@@ -1,5 +1,21 @@
 import { useEffect, useState } from 'react'
 
+export function useProducts() {
+  const [products, setProducts] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products")
+      .then((res) => res.json())
+      .then(setProducts)
+      .catch(() => setError("Erro ao carregar produtos."))
+      .finally(() => setLoading(false));
+  }, []);
+
+  return { products, loading, error };
+}
+
 export function useProduct(id) {
   const [product, setProduct] = useState(null)
   const [loading, setLoading] = useState(true)
